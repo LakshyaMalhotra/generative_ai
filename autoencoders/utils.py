dede3c195d83c8ffc5535d6f0dc8d0ef4eb09b7e
LakshyaMalhotra <lakshya9009@gmail.com>
2023-06-04 22:09:30 -0700

    Train function refactoring.

--- autoencoders/utils.py
+++ autoencoders/utils.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import torch.nn as nn
 import matplotlib.pyplot as plt
 
 
@@ -33,3 +34,43 @@ def plot_outputs(outputs: list, labels: torch.Tensor, indices: np.ndarray):
             ax[i][j].set_axis_off()
     fig.tight_layout()
     plt.show()
+
+
+def train(
+    dataloader: torch.utils.data.DataLoader,
+    epochs: int,
+    model: nn.Module,
+    device: torch.device,
+    criterion: nn.Module,
+    optimizer: torch.optim.Optimizer,
+    samples: list,
+    print_every: int = 5,
+) -> list:
+    model = model.to(device)
+    epoch_losses = []
+    best_loss = np.Inf
+    sample_outputs = []
+    for epoch in range(1, epochs + 1):
+        model.train()
+        batch_losses = []
+        for images, _ in dataloader:
+            images = images.to(device)
+            optimizer.zero_grad()
+            _, out = model(images)
+            loss = criterion(out, images)
+            loss.backward()
+            optimizer.step()
+            batch_losses.append(np.round(loss.item(), 4))
+        epoch_loss = np.round(np.mean(batch_losses), 4)
+        epoch_losses.append(epoch_loss)
+        if epoch_loss < best_loss:
+            best_loss = epoch_loss
+            torch.save(model.state_dict(), "../models/autoencoder.pth")
+        if epoch % print_every == 0:
+            model.eval()
+            sample_images = samples[0]
+            sample_images = sample_images.to(device)
+            _, outputs = model(sample_images)
+            sample_outputs.append(outputs.detach().cpu().numpy())
+            print(f"Epoch: [{epoch}/{epochs}], Loss: {epoch_loss}")
+    return sample_outputs
